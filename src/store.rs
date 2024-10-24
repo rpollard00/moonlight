21de5d29b4a5f97f1f4342c24b623e6741164a5a
Reese Pollard <rpollard@gmail.com>
2024-10-23 23:59:03 -0700

    Crud methods for family

--- src/store.rs
+++ src/store.rs
@@ -2,5 +2,4 @@ mod db;
 mod migrations;
 
 pub use db::Store;
-pub use db::UserStore;
 pub use migrations::{init_db, run_migrations};
--- src/store/db.rs
+++ src/store/db.rs
@@ -1,18 +1,22 @@
-use crate::models::User;
-use sqlx::SqlitePool;
+use std::ascii::AsciiExt;
+
+use crate::models::{Family, User};
+use sqlx::{sqlite::SqliteQueryResult, SqlitePool};
 
 #[derive(Clone)]
 pub struct Store {
     db: SqlitePool,
 
     pub users: UserStore,
+    pub family: FamilyStore,
 }
 
 impl Store {
     pub fn new(db: SqlitePool) -> Self {
         let users = UserStore::new(db.clone());
+        let family = FamilyStore::new(db.clone());
 
-        Self { db, users }
+        Self { db, users, family }
     }
 }
 
@@ -57,4 +61,59 @@ impl UserStore {
     // last_name TEXT NOT NULL,
     // email TEXT NOT NULL,
     // dob INTEGER NOT NULL,
+}
+
+#[derive(Clone)]
+struct FamilyStore {
+    db: SqlitePool,
+}
+
+impl FamilyStore {
+    pub fn new(db: SqlitePool) -> Self {
+        Self { db }
+    }
+
+    pub async fn create(&self, req: Family) -> Result<SqliteQueryResult, sqlx::Error> {
+        sqlx::query("INSERT INTO family (name) VALUES (?);")
+            .bind(req.name)
+            .execute(&self.db)
+            .await
+    }
+
+    pub async fn delete(&self, id: i64) -> Result<SqliteQueryResult, sqlx::Error> {
+        sqlx::query("DELETE FROM family WHERE id = (?);")
+            .bind(id)
+            .execute(&self.db)
+            .await
+    }
+
+    pub async fn get(&self, id: i64) -> Result<Option<Family>, sqlx::Error> {
+        sqlx::query_as::<_, Family>("SELECT id, name FROM family WHERE id = (?);")
+            .bind(id)
+            .fetch_optional(&self.db)
+            .await
+    }
+
+    pub async fn update(&self, req: Family) -> Result<SqliteQueryResult, sqlx::Error> {
+        sqlx::query("UPDATE family SET name = (?) WHERE (?);")
+            .bind(req.name)
+            .bind(req.id)
+            .execute(&self.db)
+            .await
+    }
+
+    pub async fn search(&self, query: &str) -> Result<Vec<Family>, sqlx::Error> {
+        if query.is_empty() {
+            sqlx::query_as::<_, Family>("SELECT id, name FROM family LIMIT 100;")
+                .fetch_all(&self.db)
+                .await
+        } else {
+            sqlx::query_as::<_, Family>(
+                "SELECT id, name FROM family WHERE LOWER(name) LIKE '%' || ? || '%' LIMIT 100;",
+            )
+            .bind(query.to_ascii_lowercase())
+            .fetch_all(&self.db)
+            .await
+        }
+    }
 }
