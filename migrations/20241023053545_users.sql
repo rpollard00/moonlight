41a8f9fecb35ee157244d8c148ac3c7d4413e4f9
Reese Pollard <rpollard@gmail.com>
2024-10-23 23:58:23 -0700

    Simplify and add family table

--- migrations/20241023053545_users.sql
+++ migrations/20241023053545_users.sql
@@ -1,8 +1,15 @@
 -- Add migration script here
+CREATE TABLE IF NOT EXISTS family (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY NOT NULL,
     username TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT NOT NULL
+    email TEXT NOT NULL,
+    family_id INTEGER,
+    FOREIGN KEY(family_id) REFERENCES family(id)
 );
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,17 @@
-mod user;
+use sqlx::FromRow;
 
-pub use user::User;
+#[derive(Clone, FromRow, Debug)]
+pub struct User {
+    pub id: i64,
+    pub username: String,
+    pub email: String,
+    pub first_name: String,
+    pub last_name: String,
+    pub family_id: i64,
+}
+
+#[derive(Clone, FromRow, Debug)]
+pub struct Family {
+    pub id: i64,
+    pub name: String,
+}
